f82d483f2ebfe5015ca7f455596eeadf4222d8c2
mono_komiyama <komiyama@monosus.co.jp>
2017-11-23 16:49:35 +0900

    fix

--- mono_replase_url/function.js
+++ mono_replase_url/function.js
@@ -1,7 +1,7 @@
 $(function(){
   var url = location.href;
   regExp = /file:\/\/\/C:\/.*\/ejs\/(.*)\.ejs/;
-  if(url.match(regExp)[1]){
+  if(url.match(regExp)){
     location.href = 'http://192.168.12.56:3000/' + url.match(regExp)[1] +'.html'
   }
 });
